67850bd6bcc8d557ec3c8ff582aed82efaeec79f
Masanori Tani <uta8a@uta8a.org>
2023-02-18 11:51:54 +0000

    Icon pickerを導入

--- dotfiles/source/neovim/init.lua
+++ dotfiles/source/neovim/init.lua
@@ -18,4 +18,59 @@ require("lazy").setup({
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
+  {
+    "roobert/node-type.nvim",
+    config = function()
+        require("node-type").setup()
+    end,
+  },
+  {
+  "ziontee113/icon-picker.nvim",
+  config = function()
+    require("icon-picker").setup({
+      disable_legacy_commands = true
+    })
+  end,
+  },
+  'nvim-lua/plenary.nvim',
+  { 'nvim-telescope/telescope.nvim',version='0.1.1' }, 
+  'stevearc/dressing.nvim',
+  'nvim-telescope/telescope-ui-select.nvim',
+
 })
+
+-- This is your opts table
+require("telescope").setup {
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+        -- even more opts
+      }
+
+      -- pseudo code / specification for writing custom displays, like the one
+      -- for "codeactions"
+      -- specific_opts = {
+      --   [kind] = {
+      --     make_indexed = function(items) -> indexed_items, width,
+      --     make_displayer = function(widths) -> displayer
+      --     make_display = function(displayer) -> function(e)
+      --     make_ordinal = function(e) -> string
+      --   },
+      --   -- for example to disable the custom builtin "codeactions" display
+      --      do the following
+      --   codeactions = false,
+      -- }
+    }
+  }
+}
+-- To get ui-select loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require("telescope").load_extension("ui-select")
+
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
+vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
+vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
+
